4c0e72766780b1d1d65e26a08ea0c196cee12416
tomcusack1 <tom@tom-cusack.com>
2019-10-14 20:16:28 +0100

    version bump

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ import setuptools
 
 
 setup(name='ordrbook',
-      version='0.0.1',
+      version='0.0.2',
       author='Tom Cusack-Huang',
       author_email='tom@cusack-huang.com',
       packages=['ordrbook'],
       url='https://github.com/tomcusack1/ordrbook',
-      download_url='https://github.com/tomcusack1/ordrbook/archive/v_001.tar.gz',
+      download_url='https://github.com/tomcusack1/ordrbook/dist/ordrbook-0.0.2.tar.gz',
       description='Limit Order Book',
       install_requires=['bintrees', 'blkchn'])
